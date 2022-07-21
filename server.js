bebd6e72a84cd2db367354238b580062bf7c671e
Het Bhavsar <hetjbhavsar@gmail.com>
2022-07-21 14:15:49 +0530

    changed server port

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ const routes = require('./route');
 routes(app)
 app.use(cors());
 app.use(express.json());
-
+app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+      })
