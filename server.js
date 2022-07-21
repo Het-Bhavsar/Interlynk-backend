7ea0b96ceaa352986b8367cf31aa3d3e8f0e7be6
Het Bhavsar <hetjbhavsar@gmail.com>
2022-07-21 15:20:47 +0530

    updated ther server

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 // Loads the configuration from config.env to process.env
-require('dotenv').config({ path: './config.env' });
+require("dotenv").config({ path: "./config.env" });
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 // get MongoDB driver connection
-const dbo = require('./db/conn');
+const dbo = require("./db/conn");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
-app.use(require('./routes/record'));
+app.use(require("./routes/record"));
 
-// Global error handling
-app.use(function (err, _req, res) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+app.get("/", (req, res) => {
+  res.send("Hello from Interlynk");
 });
-
 // perform a database connection when the server starts
 dbo.connectToServer(function (err) {
   if (err) {
